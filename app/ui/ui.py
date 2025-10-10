f5e48f537cf24d9c213016489ee7f744809797fa
Akash Desarda <desardaakash@gmail.com>
2025-10-10 17:48:25 +0000

    chore(app): :truck: rename to app

--- app/ui/ui.py
+++ app/ui/ui.py
@@ -1,7 +1,8 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
-from rxconfig import config
+
+from app.rxconfig import config
 
 
 class State(rx.State):

9a3c9bcb743de62d9b02a9057aeedd19ceeecdb8
Akash Desarda <desardaakash@gmail.com>
2025-12-27 19:47:04 +0530

    🚀 feat(webapp): Implement page-scoped state management
    Refactor StateManager to support multiple page-specific states
    - Add support for page-specific state initialization
    - Introduce dynamic state management with type checking
    - Improve error handling for uninitialized states
    - Add PageKey enum for consistent page key references
    
    Enhance flexibility and type safety for Streamlit app state

--- webapp/app/pages/playground/data.py
+++ webapp/app/pages/playground/data.py
@@ -17,10 +17,10 @@ from app.pages.playground._helper import (
     fetch_data_from_sql_query,
 )
 from app.state.manager import StateManager
-from app.state.model import TickerChoice
+from app.state.model import DataPageAppState, PageKey, TickerChoice
 
 settings = get_settings(os.getenv("CONFIG_FILE"))
-state = StateManager.init()
+state = StateManager.init(PageKey.data.value, DataPageAppState)
 
 st.markdown("# Data Explorer")
 st.markdown("Explore stock data and analytics.")

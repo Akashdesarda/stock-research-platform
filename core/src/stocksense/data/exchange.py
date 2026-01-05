582d09498acb265cc47b0e1aaddd438f1c71a632
Akash Desarda <desardaakash@gmail.com>
2026-01-05 19:11:47 +0530

    few minor bug fixes

--- core/src/stocksense/data/exchange.py
+++ core/src/stocksense/data/exchange.py
@@ -5,11 +5,7 @@ from ._nse import NSEAccessor
 
 @dataclass
 class Exchange:
-    name: str
-
     @property
     def nse(self) -> NSEAccessor:
         """NSE (National Stock Exchange of India) data accessor."""
-        if self.name.lower() != "nse":
-            raise ValueError("NSE accessor is only available for NSE exchange.")
         return NSEAccessor()

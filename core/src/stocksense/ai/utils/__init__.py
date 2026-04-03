dc36739089f826790444716b50c90213bb36b68e
Akash Desarda <desardaakash@gmail.com>
2026-04-03 14:00:36 +0000

    feat: add Phoenix prompt helpers and fetch utilities

--- core/src/stocksense/ai/utils/__init__.py
+++ core/src/stocksense/ai/utils/__init__.py
@@ -5,14 +5,29 @@ from ._history_processor import (
     loop_breaker_processor,
     session_ttl_processor,
 )
-from ._llm_helper import get_model, get_thinking_parts
+from ._llm_helper import (
+    get_model,
+    get_thinking_parts,
+    render_mustache_conditional_prompt,
+)
+from ._prompt_helper import (
+    fetch_prompt_by_index,
+    fetch_prompt_messages,
+    fetch_system_prompt,
+    fetch_user_prompt,
+)
 
 __all__ = [
     "get_model",
     "get_thinking_parts",
+    "render_mustache_conditional_prompt",
     "history_messages_to_json",
     "json_to_history_messages",
     "condense_old_tool_calls",
     "loop_breaker_processor",
     "session_ttl_processor",
+    "fetch_prompt_messages",
+    "fetch_system_prompt",
+    "fetch_user_prompt",
+    "fetch_prompt_by_index",
 ]
